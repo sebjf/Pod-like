893cf1840d6895dbb4a0be1622a10b7b03c8baad
Sebastian Friston <sebastian.friston@gmail.com>
2019-02-18 22:48:03 +0000

    updated gravity and added measure tool

---
+++ Assets/Tools/MeasureTool.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MeasureTool : MonoBehaviour
+{
+    public Transform from;
+    public Transform to;
+
+    public float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(from)
+        {
+            if(to)
+            {
+                distance = (from.position - to.position).magnitude;
+            }
+        }
+    }
+}
