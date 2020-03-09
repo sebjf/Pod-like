7b239dba767ffa1e701b6eecb39898d925216a5c
Sebastian Friston <sebastian.friston@gmail.com>
2020-03-09 17:50:13 +0000

    added script to enable capturetime for non-realtime simulation

---
+++ Assets/AI/TimeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    public bool enableCapture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enableCapture)
+        {
+            Time.captureFramerate = 60;
+        }
+        else
+        {
+            Time.captureFramerate = 0;
+        }
+    }
+}
