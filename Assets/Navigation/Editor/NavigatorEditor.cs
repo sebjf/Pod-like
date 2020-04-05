b8f48a8c7109c8d792af2e33a04b095bf484fc51
Sebastian Friston <sebastian.friston@gmail.com>
2020-04-05 18:48:04 +0100

    added inference version of pathfinder that uses trained nn to drive. tested on alderon.

---
+++ Assets/Navigation/Editor/NavigatorEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Navigator))]
+public class NavigatorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("waypoints"));
+        serializedObject.ApplyModifiedProperties();
+
+        var navigator = target as Navigator;
+
+        EditorGUILayout.LabelField("Distance", navigator.TrackDistance.ToString());
+
+    }
+}
