e2cc03ae4b421c92390f9d95eb9f4d3afa9c7e6c
Sebastian Friston <sebastian.friston@gmail.com>
2020-04-09 16:03:04 +0100

    allowed path to be selected from dropdown

--- Assets/Navigation/Editor/NavigatorEditor.cs
+++ Assets/Navigation/Editor/NavigatorEditor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.Linq;
 
 [CustomEditor(typeof(Navigator))]
 public class NavigatorEditor : Editor
@@ -9,7 +10,22 @@ public class NavigatorEditor : Editor
     public override void OnInspectorGUI()
     {
         serializedObject.Update();
-        EditorGUILayout.PropertyField(serializedObject.FindProperty("waypoints"));
+
+        var paths = new Dictionary<string, TrackPath>();
+        foreach (var item in (target as Navigator).GetComponentsInParent<TrackPath>())
+        {
+            paths.Add(TrackPathName(item), item);
+        }
+        var pathNames = paths.Keys.ToList();
+        var selected = serializedObject.FindProperty("waypoints").objectReferenceValue as TrackPath;
+        var selectedIndex = pathNames.IndexOf(TrackPathName(selected));
+        var newSelectedIndex = EditorGUILayout.Popup(selectedIndex, pathNames.ToArray());
+        if(selectedIndex != newSelectedIndex)
+        {
+            serializedObject.FindProperty("waypoints").objectReferenceValue = paths[pathNames[newSelectedIndex]];
+
+        }
+
         serializedObject.ApplyModifiedProperties();
 
         var navigator = target as Navigator;
@@ -17,4 +33,14 @@ public class NavigatorEditor : Editor
         EditorGUILayout.LabelField("Distance", navigator.TrackDistance.ToString());
 
     }
+
+    private static string TrackPathName(TrackPath item)
+    {
+        if(item == null)
+        {
+            return null;
+        }
+        return item.name + " " + item.GetType().Name;
+    }
+
 }
