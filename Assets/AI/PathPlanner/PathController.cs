8736b2c67e9a1d1e9b8bfff853cc1c7d6411e1ef
Sebastian Friston <sebastian.friston@gmail.com>
2020-07-08 21:51:11 +0100

    added path controller that randomly initialises for now

---
+++ Assets/AI/PathPlanner/PathController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Vehicle))]
+public class PathController : MonoBehaviour
+{
+    private Navigator navigator;
+
+    private void Awake()
+    {
+        navigator = GetComponent<Navigator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(navigator.waypoints == null)
+        {
+            var path = gameObject.AddComponent<InterpolatedPath>();
+            path.coefficient = Random.value;
+            path.Initialise();
+            navigator.waypoints = path;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
