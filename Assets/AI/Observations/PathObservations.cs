b490d9495a4ae2ff0276af834f6792542e7efffc
Sebastian Friston <sebastian.friston@gmail.com>
2020-07-08 21:49:24 +0100

    fixes for robustness in pathobservatoins

--- Assets/AI/Observations/PathObservations.cs
+++ Assets/AI/Observations/PathObservations.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Profiling;
@@ -34,6 +35,9 @@ public class PathObservations : MonoBehaviour
 
     [HideInInspector]
     public float directionError;
+
+    [HideInInspector]
+    public float direction;
 
     [HideInInspector]
     public bool jumprulesflag;
@@ -75,32 +79,30 @@ public class PathObservations : MonoBehaviour
         var M = new Vector2(bodyPosition.x, bodyPosition.z);
         var T = new Vector2(q.Tangent.x, q.Tangent.z);
 
+        var side = Mathf.Sign((B.x - A.x) * (M.y - A.y) - (B.y - A.y) * (M.x - A.x));
+
         lateralError = Mathf.Abs(Vector3.Dot(M - A, T));
 
-        var side = Mathf.Sign((B.x - A.x) * (M.y - A.y) - (B.y - A.y) * (M.x - A.x));
+        if (float.IsInfinity(curvature))
+        {
+            curvature = -Math.Sign(side); // assume oversteering on perfect straights
+        }
 
         if(side != Mathf.Sign(curvature))
         {
             understeer = 0;
-            oversteer = lateralError;
+            oversteer = Mathf.Abs(lateralError);
         }
         else
         {
             oversteer = 0;
-            understeer = lateralError;
+            understeer = Mathf.Abs(lateralError);
         }
 
         Profiler.EndSample();
         Profiler.BeginSample("Sideslip Angle");
 
-        sideslipAngle = 0;
-        foreach (var item in vehicle.wheels)
-        {
-            if(item.inContact)
-            {
-                sideslipAngle += item.sideslipAngle;
-            }
-        }
+        sideslipAngle = vehicle.sideslipAngle;
 
         Profiler.EndSample();
         Profiler.BeginSample("Height");
@@ -120,14 +122,12 @@ public class PathObservations : MonoBehaviour
         traction = vehicle.wheelsInContact > 3;
         speed = body.velocity.magnitude;
 
-        var forward = trackForward;
-        forward.y = 0;
-        forward.Normalize();
+        var projectedVehicleForward = new Vector3(vehicle.transform.forward.x, 0f, vehicle.transform.forward.z).normalized;
+        var projectedTrackForward = new Vector3(trackForward.x, 0f, trackForward.z).normalized;
 
-        var projectedVehicle = new Vector3(vehicle.transform.forward.x, 0f, vehicle.transform.forward.z).normalized;
-        var projectedTrack = new Vector3(trackForward.x, 0f, trackForward.z).normalized;
+        direction = Vector3.Dot(projectedVehicleForward, projectedTrackForward);
         
-        directionError = (1 - Vector3.Dot(projectedTrack, projectedVehicle)) * Mathf.Sign(Vector3.Dot(Vector3.Cross(projectedTrack, projectedVehicle), Vector3.up)); 
+        directionError = (1 - Vector3.Dot(projectedTrackForward, projectedVehicleForward)) * Mathf.Sign(Vector3.Dot(Vector3.Cross(projectedTrackForward, projectedVehicleForward), Vector3.up)); 
     }
 
     /// <summary>
@@ -149,14 +149,22 @@ public class PathObservations : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.layer == trackLayerMask)
+        for (int i = 0; i < collision.contactCount; i++)
         {
-            for (int i = 0; i < collision.contactCount; i++)
+            if (Mathf.Abs(Vector3.Dot(collision.GetContact(i).normal, Vector3.up)) < 0.7f)
             {
-                if (Mathf.Abs(Vector3.Dot(collision.GetContact(i).normal, Vector3.up)) < 0.5f)
-                {
-                    collisionOccurred = true;
-                }
+                collisionOccurred = true;
+            }
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            if (Mathf.Abs(Vector3.Dot(collision.GetContact(i).normal, Vector3.up)) < 0.7f)
+            {
+                collisionOccurred = true;
             }
         }
     }
