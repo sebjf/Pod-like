f90ad5ebcdc3950b7fe800c393daed068036d028
Sebastian Friston <sebastian.friston@gmail.com>
2020-07-08 21:59:00 +0100

    added new circuit class to replace binary from exporter and updated circuit settings

---
+++ Assets/Circuits/Circuit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(menuName = "Assets/Circuit Data")]
+public class Circuit : ScriptableObject
+{
+    public string Name;
+    public string sceneName;
+}
