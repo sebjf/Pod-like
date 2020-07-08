45c3dfd5807de031528305a6e38ebad1abf81819
Sebastian Friston <sebastian.friston@gmail.com>
2020-07-08 22:00:13 +0100

    added new catalogue functionality for recording available cars and tracks along with their relevant objects at design and runtime

---
+++ Assets/Scripts/Catalogue.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Assets/Catalogue")]
+public class Catalogue : ScriptableObject
+{
+    public List<Car> cars;
+    public List<Circuit> circuits;
+}
