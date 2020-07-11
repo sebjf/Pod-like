5e21cfe5f98855484a27d68814353f2ce85c49ed
Sebastian Friston <sebastian.friston@gmail.com>
2020-07-11 15:57:04 +0100

    added proto leaderboard to racing launcher scene

---
+++ Assets/Scripts/UI/LeaderboardController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeaderboardController : MonoBehaviour
+{
+    public Leaderboard leaderboard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string rtf = "";
+        foreach (var item in leaderboard.drivers)
+        {
+            rtf += item.vehicle.gameObject.name + "\n";
+        }
+        GetComponent<Text>().text = rtf;
+    }
+}
