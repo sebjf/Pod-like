0f3b3d04142181e2eb11b881b99bef15804055b9
Sebastian Friston <sebastian.friston@gmail.com>
2020-07-23 20:52:28 +0100

    added race camera editor

---
+++ Assets/Scripts/Editor/RaceCameraEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+[CustomEditor(typeof(RaceCamera))]
+public class RaceCameraEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        var component = target as RaceCamera;
+
+        if (component.cameraRigs != null) {
+            for (int i = 0; i < Mathf.Min(component.cameraRigs.Length, 10); i++)
+            {
+                if(GUILayout.Button(component.cameraRigs[i].name))
+                {
+                    component.Target = component.cameraRigs[i];
+                }
+            }
+        }
+    }
+}
